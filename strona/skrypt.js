795c125357da7c725087c5b3403b65814e04571f
Szymon Lato <szymek.lato@gmail.com>
2024-10-26 23:15:49 +0200

    seks

---
+++ strona/skrypt.js
@@ -0,0 +1,37 @@
+let licznik = 0;
+const maslo = "SmokiILochy420"
+
+const modalSrodal = () => {
+    document.getElementById("modal").style.display = "flex";
+};
+
+const zamykamyModalNieJestesmyDorszem = () => {
+    document.getElementById("modal").style.display = "none";
+    document.getElementById("password").value = "";
+};
+
+const sprawdzanko = () => {
+    const wpisane = document.getElementById("password").value
+    if (wpisane === maslo) {
+        licznik++
+        document.getElementById("counter").innerText = licznik;
+        zamykamyModalNieJestesmyDorszem();
+    } else {
+        alert("A NIE DLA PSA DLA PANA TO");
+    }
+}
+
+window.onclick = function(event) {
+    const modal = document.getElementById("modal");
+    if (event.target === modal) {
+        zamykamyModalNieJestesmyDorszem();
+    }
+}
+
+// window.onclick = function(event) {
+//     const modal = document.getElementById("modal");
+//     // Sprawdź, czy kliknięto poza modalem
+//     if (event.target === modal) {
+//         zamykamyModalNieJestesmyDorszem();
+//     }
+// };
